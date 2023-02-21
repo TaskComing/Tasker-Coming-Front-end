adbdd94cb6c6a17b93346f409d64829336817063
OwenHuang8 <123640783+OwenHuang8@users.noreply.github.com>
2023-02-21 21:57:06 +1100

    feat: add side nav (#8)

---
+++ src/components/ProfileSider.js
@@ -0,0 +1,60 @@
+import { styled } from '@mui/system';
+import Avatar from '@mui/material/Avatar';
+import Badge from '@mui/material/Badge';
+import Typography from '@mui/material/Typography';
+import SiderTab from './SiderTab';
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  '& .MuiBadge-badge': {
+    backgroundColor: '#44b700',
+    color: '#44b700',
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    width: '20px',
+    height: '18.57px',
+    borderRadius: '50%',
+  },
+  '@keyframes ripple': {
+    '0%': {
+      transform: 'scale(.8)',
+      opacity: 1,
+    },
+    '100%': {
+      transform: 'scale(2.4)',
+      opacity: 0,
+    },
+  },
+}));
+
+const SiderContainer = styled('div')(({ theme }) => ({
+  backgroundColor: theme.palette.primary.sider,
+  width: '375px',
+  height: '703px',
+  borderRadius: '4px',
+  padding: '36px 72px 21px 81px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: '20px',
+}));
+
+function ProfileSider({ ...props }) {
+  return (
+    <SiderContainer>
+      <StyledBadge
+        overlap="circular"
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        variant="dot"
+      >
+        <Avatar
+          alt="Remy Sharp"
+          src="img/defaultAvatar.svg"
+          sx={{ width: '120px', height: '120px' }}
+        />
+      </StyledBadge>
+      <Typography variant="h1">User&apos;s Name</Typography>
+      <SiderTab {...props} />
+    </SiderContainer>
+  );
+}
+
+export default ProfileSider;
---
+++ src/pages/EditProfile.js
@@ -0,0 +1,55 @@
+import { styled, createTheme, ThemeProvider } from '@mui/material';
+import * as React from 'react';
+
+import ProfileSider from '../components/ProfileSider';
+import ProfileSettingBox from '../components/ProfileSettingBox';
+
+const customTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#f5f5f5',
+      sider: '#E6EFEC',
+    },
+    secondary: {
+      main: '#4C535F',
+    },
+  },
+  typography: {
+    h1: {
+      color: '#353535',
+      fontWeight: 600,
+      fontSize: '30px',
+      lineHeight: '45px',
+    },
+    h2: {
+      color: '#4C535F',
+      fontWeight: 700,
+      fontSize: '36px',
+      lineHeight: '54px',
+    },
+  },
+});
+
+const MainContainer = styled('div')(({ theme }) => ({
+  backgroundColor: theme.palette.primary.main,
+  display: 'flex',
+  padding: '102px 53px 138px 63px',
+  gap: '57px',
+}));
+
+function EditProfile() {
+  const [value, setValue] = React.useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  return (
+    <ThemeProvider theme={customTheme}>
+      <MainContainer>
+        <ProfileSider value={value} handleChange={handleChange} />
+        {value === 2 && <ProfileSettingBox />}
+      </MainContainer>
+    </ThemeProvider>
+  );
+}
+
+export default EditProfile;
