e2d51a0f40505f498d134704c60d44d0dfbece63
OwenHuang8 <123640783+OwenHuang8@users.noreply.github.com>
2023-02-27 21:19:01 +1030

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Theme from './Theme/Theme';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './pages/Layout';
 import Home from './pages/homepage/Home';
@@ -12,20 +13,22 @@ import EditProfile from './pages/EditProfile';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="post-task" element={<PostTask />} />
-          <Route path="browse-task" element={<BrowseTasks />} />
-          <Route path="about-team" element={<AboutTeam />} />
-          <Route path="login" element={<Login />} />
-          <Route path="create-account" element={<CreateAccount />} />
-          <Route path="*" element={<Error />} />
-          <Route path="profile" element={<EditProfile />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={Theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="post-task" element={<PostTask />} />
+            <Route path="browse-task" element={<BrowseTasks />} />
+            <Route path="about-team" element={<AboutTeam />} />
+            <Route path="login" element={<Login />} />
+            <Route path="create-account" element={<CreateAccount />} />
+            <Route path="*" element={<Error />} />
+            <Route path="profile" element={<EditProfile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
