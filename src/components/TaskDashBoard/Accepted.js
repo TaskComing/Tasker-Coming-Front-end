ee9ad6101e62c424836ca281dc193e718b22cf01
Siying Qian <qiansy0205@gmail.com>
2023-02-28 22:18:06 +1000

    task-dashboard-change1 (haven't make change)

---
+++ src/components/TaskDashBoard/Accepted.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './TaskDashboardStyle.css';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import TaskData from './TaskData';
+
+function Posted() {
+  return (
+    <>
+      {TaskData.filter((task) => task.category === 'Accepted').map((task, index) => (
+        <div key={index} className="box">
+          <img src={task.image} alt="task_picture" className="img" />
+          <div className="description">
+            <h3>{task.title}</h3>
+            <p>{task.location}</p>
+            <p>{task.date}</p>
+            <p>{task.time}</p>
+          </div>
+          <div className="status_box">
+            <p className="price">{task.price}</p>
+            <p className="status">{task.status}</p>
+          </div>
+          <span className="icon">
+            <ChevronRightIcon fontSize="large" />
+          </span>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default Posted;
