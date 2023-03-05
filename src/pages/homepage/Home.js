ffd521e99eea7185dd4dc184ddc937b9e1f12570
maggieAU1314 <lavendermaggie1314@outlook.com>
2023-03-05 22:01:44 +1100

    commit 2

--- src/pages/homepage/Home.js
+++ src/pages/homepage/Home.js
@@ -62,75 +62,77 @@ const theme = createTheme({
       marginTop: '6%',
     },
   },
-};
+});
 
 function Home() {
   return (
     <div className="Homepage">
       <Container className="bgColor">
-        {/** section 1 */}
-        <Box className="section1">
-          <Typography variant="h1" component="h2" sx={homepageStyles.h1}>
-            90% Clients Hire Freelancer with Better Reviews
-          </Typography>
-          <Button
-            variant="contained"
-            color="success"
-            className="btn"
-            sx={{ bgcolor: 'button.purpleBtn', fontSize: '1.2rem' }}
-          >
-            Become a Tasker
-          </Button>
-        </Box>
-
-        {/** section 2 */}
-        <Box className="textCenter">
-          <Typography variant="h2" component="h2" sx={homepageStyles.h2}>
-            Everyday life made easier
-          </Typography>
-          <Typography variant="description" component="p" sx={homepageStyles.subtitle1}>
-            When life gets busy, you don’t have to tackle it alone. Get time back for what you love
-            without breaking the bank.
-          </Typography>
-        </Box>
-
-        <Grid container spacing={5} marginTop={10}>
-          <Grid xs={6} item>
-            <img className="bg1" src="/img/bg1.png" alt="icon" />
-          </Grid>
-
-          <Grid xs={6} container spacing={3}>
-            <Card />
-          </Grid>
-        </Grid>
-
-        {/** section 3 */}
-
-        <Grid container rowSpacing={5} columnSpacing={2} marginTop={5} marginBottom={10}>
-          <Grid xs={12} item>
-            <Typography variant="h2" component="h2" sx={homepageStyles.section3}>
-              Explore by Category
+        <ThemeProvider theme={theme}>
+          {/** section 1 */}
+          <Box className="section1">
+            <Typography variant="h1" component="h2">
+              90% Clients Hire Freelancer with Better Reviews
             </Typography>
-            <Typography
-              variant="description"
-              component="p"
-              sx={{
-                textAlign: 'center',
-                margin: 'auto',
-                marginTop: '4%',
-                color: 'font.darkGrey',
-              }}
-            >
-              Find the right Tasker for your job. From home cleaning to handyman services and more,
-              Task Huntly has you covered.
+            <Button variant="contained" color="success" className="btn">
+              Become a Tasker
+            </Button>
+          </Box>
+
+          {/** section 2 */}
+          <Box className="textCenter">
+            <Typography variant="h2" component="h2">
+              Everyday life made easier
             </Typography>
+            <Typography variant="subtitle1" component="p">
+              When life gets busy, you don’t have to tackle it alone. Get time back for what you
+              love without breaking the bank.
+            </Typography>
+          </Box>
+
+          <Grid container spacing={5} marginTop={10}>
+            <Grid xs={6} item>
+              <img className="bg1" src="/img/bg1.png" alt="icon" />
+            </Grid>
+
+            <Grid xs={6} container spacing={3}>
+              <Card />
+            </Grid>
           </Grid>
 
-          <Grid container margin={5}>
-            <Category />
+          {/** section 3 */}
+
+          <Grid container rowSpacing={5} columnSpacing={2} marginTop={5} marginBottom={10}>
+            <Grid xs={12} item>
+              <Typography
+                variant="h2"
+                component="h2"
+                sx={{
+                  textAlign: 'center',
+                  margin: 'auto',
+                  marginTop: '6%',
+                }}
+              >
+                Explore by Category
+              </Typography>
+              <Typography
+                variant="subtitle1"
+                component="p"
+                sx={{
+                  textAlign: 'center',
+                  margin: 'auto',
+                  marginTop: '4%',
+                }}
+              >
+                Find the right Tasker for your job. From home cleaning to handyman services and
+                more, Task Huntly has you covered.
+              </Typography>
+            </Grid>
+
+            <Grid container margin={5}>
+              <Category />
+            </Grid>
           </Grid>
-        </Grid>
-        
           {/** section 4 */}
           <Box className="textCenter">
             <Typography variant="subtitle2" component="p">
