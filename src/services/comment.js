7f5129b6ae6da05536079a02784b5a8b23e50109
L80W1998 <L80W1998@gmail.com>
2023-04-01 13:49:45 +1000

    before merge 0104

---
+++ src/services/comment.js
@@ -0,0 +1,12 @@
+// import http from '../utils/axios';
+
+// export const addComment = (text) =>
+//   http(`/v1/comments`, {
+//     method: 'POST',
+//     data: {
+//       _id: 102,
+//       title,
+//       state,
+//       deteled: false,
+//     },
+//   });
